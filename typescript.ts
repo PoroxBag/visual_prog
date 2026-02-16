03da82357d7944696172abd1844c836cbd258617
Журов Дмитрий Евгеньевич ИП-413 <dimasilo1@yandex.ru>
2026-02-16 22:44:10 +0700

    Реализован интерфейс Book и функция CreateBook

--- typescript.ts
+++ typescript.ts
@@ -19,3 +19,31 @@ console.log(user1);
 const user2 = createUser(2, 'SnailBob', undefined, false);
 
 console.log(user2);
+
+type Genre = 'fiction' | 'non-fiction';
+
+interface Book {
+    title: string;
+    author: string;
+    year?: number;
+    genre: Genre;
+}
+
+function createBook(book: Book): Book {
+    return book;
+}
+
+const book1: Book = {
+    title: 'Metro 2033',
+    author: 'Dmitry Glukhovskiy',
+    year: 2005,
+    genre: 'fiction'
+};
+console.log(createBook(book1));
+
+const book2: Book = {
+    title: 'FNaF Silver Eyes',
+    author: 'Scott Cowton',
+    genre: 'non-fiction'
+};
+console.log(createBook(book2));
